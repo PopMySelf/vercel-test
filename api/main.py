244411fb3169c7cde47d9e6f4e32d2571e6ee037
PopMySelf <cv.ok.ddz@gmail.com>
2023-09-08 13:15:46 +0300

    Fix

--- api/main.py
+++ api/main.py
@@ -16,20 +16,16 @@ def handle_telegram():
     if request.content_type == 'application/json' and (
             update := types.Update.de_json(request.stream.read().decode('utf-8'))
     ).message and update.message.from_user.id in [652015662]:
-        bot.send_message(
-            652015662,
-            f'{type(update.message.from_user.id)}\n{time.perf_counter() - t = }',
-            timeout=5
-        )
-        bot.process_new_updates([update])
+        handle(update.message)
+        # bot.process_new_updates([update])
 
     return ''
 
 
-@bot.message_handler()
+# @bot.message_handler()
 def handle(m: types.Message):
     try:
-        bot.send_message(m.chat.id, f'{time.perf_counter() - t = }', timeout=5)
+        bot.send_message(m.chat.id, f'From handle\n{time.perf_counter() - t = }', timeout=5)
     except Exception:
         bot.send_message(m.chat.id, format_exc())
 
